94a5158de3ef66ef15cab3971a7dd4b5950c206c
Halil Ceyhan <halilceyhan05@gmail.com>
2023-05-06 19:59:46 +0300

    add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
 	// Enable Solid to support Solid JSX components.
-	integrations: [solid()],
+	integrations: [mdx(), solid()],
 });
